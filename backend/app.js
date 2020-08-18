1cd96ade2a432cd25bdef092a6ee19f882820d70
Jon-Erik Klint <jonerik.klint@gmail.com>
2020-08-18 17:40:39 +0300

    keskustelun alku

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require("cors")
 
 const measuresRouter = require("./controllers/measures")
 const measurementsRouter = require("./controllers/measurements")
 
 const app = express()
 
+app.use(cors())
 app.use(bodyParser.json())
 
 app.use("/api/measures", measuresRouter)
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 
 import App from "./components/App"
 
+import axios from "axios"
+
+const promise = axios.get("http://localhost:3001/api/measures")
+promise.then(data => console.log(data))
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
